c92667d18f389d1a63185d78e6ba639e44ec2703
InkLao <eduardo.cornejo.balcazar@gmail.com>
2024-06-06 21:08:45 -0700

    Cinepolis
    Creación del repositorio

---
+++ src/main/java/com/mycompany/cinepolis_bda/Cinepolis_bda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cinepolis_bda;
+
+/**
+ *
+ * @author eduar
+ */
+public class Cinepolis_bda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
