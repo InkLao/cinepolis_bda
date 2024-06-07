83f5d93b4778de0b522d3d48b5fb93062b0bb625
InkLao <eduardo.cornejo.balcazar@gmail.com>
2024-06-07 03:44:51 -0700

    Creación de la Conexión a la base de datsos
    - Clases IConexionBD y ConexionBD realizadas
    Nota: El usuario y contraseña no es el final, será modificado a una que tengamos en común todos los participantes

---
+++ src/main/java/persistencia/IConexionBD.java
@@ -0,0 +1,12 @@
+package persistencia;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 
+ * @author eduar
+ */
+public interface IConexionBD {
+    public Connection crearConexion() throws SQLException;
+}
